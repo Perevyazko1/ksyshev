a8fa2d73ed2b9d6243f47fa5d0638024082430bc
Perevyazko <a.perevyazko@gmail.com>
2023-01-24 20:47:57 +0300

    add picture in admin-panel

--- project/ksyshev/admin.py
+++ project/ksyshev/admin.py
@@ -5,6 +5,10 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     model = Post
+    list_display = ('date_load', 'get_html_photo')
+
+    def get_html_photo(self, object):
+        return mark_safe(f"<img src='{object.image.url}'width=50")
 
 
 admin.site.register(Post, PostAdmin)
