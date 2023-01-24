c8f15b6b983220b84f7a62d5a676add1dd07e29d
Perevyazko <a.perevyazko@gmail.com>
2023-01-24 15:33:12 +0300

    insert picture

--- project/ksyshev/models.py
+++ project/ksyshev/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Post(models.Model):
     date_load = models.DateTimeField(auto_now_add=True, verbose_name='Дата загрузки')
-    image = models.ImageField(upload_to='images/', default=None)
+    image = models.ImageField(upload_to='static/images/', default=None)
