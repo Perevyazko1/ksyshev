71334a9fb0dcde6c560c6e23c7b84cf069f93099
Perevyazko <a.perevyazko@gmail.com>
2023-01-24 15:09:07 +0300

    Corrected html photo

--- project/ksyshev/urls.py
+++ project/ksyshev/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from .views import home
+from .views import home, PhotoList
 
 urlpatterns = [
     path('', home, name='home'),
+    path('photo/', PhotoList.as_view(), name='photo'),
 ]
--- project/ksyshev/views.py
+++ project/ksyshev/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, ListView
+
+from .models import Post
 
 
 def home(request):
@@ -7,4 +9,12 @@ def home(request):
     Представление, для главной страницы.
     """
     return render(request, 'index.html')
+
+
+class PhotoList(ListView):
+    raise_exception = True
+    model = Post
+    template_name = 'photo.html'
+    context_object_name = 'photo'
+
 
