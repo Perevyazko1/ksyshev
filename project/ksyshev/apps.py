568f0e050b58499cb4b841f8cf43fb28b6a602a4
Perevyazko <a.perevyazko@gmail.com>
2023-01-29 13:58:21 +0300

    add signal

--- project/ksyshev/apps.py
+++ project/ksyshev/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class KsyshevConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ksyshev'
+
+    def ready(self):
+        from . import signals
---
+++ project/ksyshev/signals.py
@@ -0,0 +1,17 @@
+from .models import Post, Collage
+from django.dispatch import receiver
+from django.db.models.signals import m2m_changed, post_delete, post_save, pre_delete
+
+
+
+
+@receiver(pre_delete, sender=Post)
+def image_delete(sender, instance, **kwargs):
+    if instance.image.name:
+        instance.image.delete(False)
+
+
+@receiver(pre_delete, sender=Collage)
+def collage_delete(sender, instance, **kwargs):
+    if instance.image.name:
+        instance.image.delete(False)
