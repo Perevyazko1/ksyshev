255bc2cdbe71b91d4c88471ed4177c168cd107e5
Perevyazko <a.perevyazko@gmail.com>
2023-02-07 11:12:08 +0300

    setting admin panel and template cintact

--- project/ksyshev/models.py
+++ project/ksyshev/models.py
@@ -37,3 +37,17 @@ class Video(models.Model):
 
     def __str__(self):
         return f'{self.date_load}| {self.video}'
+
+
+
+
+class Ip(models.Model): # наша таблица где будут айпи адреса
+    class Meta:
+        verbose_name = u"Посещение"
+        verbose_name_plural = u"Посещения"
+
+    ip = models.CharField(max_length=100)
+    date_visit = models.DateTimeField(auto_now=True, verbose_name='Дата посещения')
+
+    def __str__(self):
+        return self.ip
--- project/ksyshev/views.py
+++ project/ksyshev/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView
 
-from .models import Post, Collage
+from .models import Post, Collage, Ip
 
 
 # def home(request):
@@ -29,7 +29,33 @@ class PhotoDetail(DetailView):
     context_object_name = 'photo'
 
 
+# Метод для получения айпи
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR') # В REMOTE_ADDR значение айпи пользователя
+    return ip
+
+
+
 def contact(request):
+    ip = get_client_ip(request)
+    # if request.method == 'POST':
+    #     form = SendMailForm(request.POST)
+    #     data = request.POST.get('name')
+    #
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    # else:
+    #     form = SendMailForm()
+    #     print(form)
+    if Ip.objects.filter(ip=ip).exists():
+        ip_save=Ip.objects.filter(ip=ip)
+        ip_save.update(ip=ip)
+    else:
+        Ip.objects.create(ip=ip)
     return render(request, 'contact.html')
 
 
