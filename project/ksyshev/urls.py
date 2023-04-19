8735068c75e000a4eed34a3afa7fe6500ba64a2e
Perevyazko <a.perevyazko@gmail.com>
2023-04-19 07:31:31 +0300

    refactoring

--- project/ksyshev/urls.py
+++ project/ksyshev/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from .views import Home, PhotoList, PhotoDetail, video, contact
+from .views import Home, PhotoList, PhotoDetail, VideoList, contact
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('video/', video, name='video'),
+    path('video/', VideoList.as_view(), name='video'),
     path('contact/', contact, name='contact'),
     path('photo/', PhotoList.as_view(), name='photo'),
     path('<int:pk>', PhotoDetail.as_view(), name='photo_detail'),
